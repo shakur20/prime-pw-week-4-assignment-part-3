de9e0ddd5572a6d96b816c6e990da2a70469b7eb
mohamed shakur <hanad@mohameds-MacBook-Pro.local>
2021-10-17 17:38:40 -0500

    completed assignment

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,22 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+function addItem(item) {
+  console.log("in addItem", item);
+  basket.push(item);
+  return true;
+}
+console.log("in addItem", addItem("item"));
+
+function listItems() {
+  console.log("in listItems", basket);
+  for (let i = 0; i < basket.length; i++) return listItems;
+}
+console.log("in listItems", listItems());
+function empty() {
+  basket.splice(0, basket.length);
+  return true;
+}
+console.log(empty());
+console.log(basket.length);
